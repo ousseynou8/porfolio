350734e29fbbbfa41147adde658fed13ff66a592
ousseynou8 <ousseynoufofana15@gmail.com>
2020-12-08 22:09:15 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+
+
+// Ecrit Defiler
+var typed = new Typed("#typed", {
+  strings: ["Intégrateur", "Développeur", "Concepteur"],
+  typedSpeed: 0,
+  backSpeed: 70,
+  loop: true,
+});
+
+
+ 
+// let form = document.getElementById('form');
+
+// form.addEventListener('sumbit', function(e){
+//     e.preventDefault();
+//     console.log('after submit');
+
+// prenomValeur = prenom.value;
+// console.log(prenomValeur);
+
+// nomValeur = nom.value;
+// console.log(nomValeur);
+
+// emailValeur = email.value;
+// console.log(emailValeur);
+
+
+// // Validation
+// if (prenomValeur == "" || nomValeur == "" || emailValeur == "") {
+//     console.log('erreur');
+//     alertBox.style.display = "block";
+// } else {
+//     window.location = 'confirmation.html';
+// }
+
+
+// })
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar'
+})
+
+
+
+new WOW().init();
---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  "use strict";
+  // Eau
+  $(".slider").ripples({
+    resoluion: 512,
+    dropRadius: 11,
+    perturbance: 0.1,
+  });
+
+  $("#year").text(new Date().getFullYear());
+
+  // Scroll
+  $(window).scroll(function () {
+    $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
+  });
+
+  // boutonHaut
+  $(function () {
+    $("#monBouton").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 3000);
+    });
+  });
+
+
+// $(function(){
+//   var scroll = document.body.scrollTop&&document.documentElement;
+//       $(document).scroll(function() {
+//         if($('html').scrollTop() > 130 || $('body').scrollTop() > 130)
+//      $("#image").css('visibility', 'visible').fadeInt(300);
+//       else
+//      $("#image").css('visibility', 'hidden').fadeOut(100);
+//       });
+//   });
+
+
+ 
+
+});
