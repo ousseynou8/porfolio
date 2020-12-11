a58938e3fb3585d6f03f5adf29cac7fbbfd87a7f
ousseynou8 <ousseynoufofana15@gmail.com>
2020-12-11 11:45:17 +0100

    azerty

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
   // boutonHaut
   $(function () {
     $("#monBouton").click(function () {
-      $("html, body").animate({ scrollTop: 0 }, 1000);
+      $("html, body").animate({ scrollTop: 0 }, 3000);
     });
   });
 
